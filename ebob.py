6fe3916feea1572cd8d5efd1db61e3b1b3c52f98
İzzet TEKBAŞ <87038595+izolas@users.noreply.github.com>
2022-02-10 23:36:39 +0300

    Create ebob.py
    deneme

---
+++ ebob.py
@@ -0,0 +1,12 @@
+def ebob(x,y):
+    temp=1
+    for i in range(1,min(x,y)+1):
+        if (x%i==0 and y%i==0):
+            x /=i
+            y /=i
+            temp *=i
+    return temp
+
+x=int(input("ilk sayıyı giriniz:"))
+y=int(input("\nikinci sayıyı giriniz:"))
+print("\n",x," ve ",y,"sayılarının EBOB'u:",ebob(x,y))
