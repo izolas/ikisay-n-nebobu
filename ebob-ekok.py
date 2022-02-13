3f7355ff2e442b51221a585af4743b806f0bfc45
İzzet TEKBAŞ <87038595+izolas@users.noreply.github.com>
2022-02-13 17:09:39 +0300

    Update and rename ebob.py to ebob-ekok.py

--- ebob-ekok.py
+++ ebob-ekok.py
@@ -7,6 +7,16 @@ def ebob(x,y):
             temp *=i
     return temp
 
+def ekok(x,y):
+    temp2=1
+    for i in range(1,min(x,y)+1):
+        if (x%i==0 and y%i==0):
+            x /=i
+            y /=i
+            temp2 *=i
+    return temp2*x*y
+
 x=int(input("ilk sayıyı giriniz:"))
 y=int(input("\nikinci sayıyı giriniz:"))
 print("\n",x,"ve",y,"sayılarının EBOB'u:",ebob(x,y))
+print("\n {} ve {} sayılarının EKOK'u: {:.0f}".format(x,y,ekok(x,y)))
